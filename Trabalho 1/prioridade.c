fa84e946780b7482ef7a519b6daee3dd1c868455
Guilherme Salomao <guime.sa9@gmail.com>
2021-04-30 19:34:59 -0300

    Arrumamos o problema dos arquivos pararem de executar misteriosamente, e aproveitamos para identar

--- Trabalho 1/prioridade.c
+++ Trabalho 1/prioridade.c
@@ -1,9 +1,9 @@
 /*
 INTEGRANTES:
-Nome: Eduardo Marques da Silva          - Número: 11721EMT018
-Nome: Enrico Sampaio Bonela             - Número: 11721EMT007
-Nome: Guilherme Salomao Agostini        - Número: 11721EMT003
-Nome: Luiz Renato Rodrigues Carneiro    - Número: 11721EMT004
+Nome: Eduardo Marques da Silva - Número: 11721EMT018
+Nome: Enrico Sampaio Bonela - Número: 11721EMT007
+Nome: Guilherme Salomao Agostini - Número: 11721EMT003
+Nome: Luiz Renato Rodrigues Carneiro - Número: 11721EMT004
 */
 
 #include <stdio.h>
@@ -13,25 +13,23 @@ Nome: Luiz Renato Rodrigues Carneiro    - Número: 11721EMT004
 #include "type.h"
 #include "cpu.c"
 #include "prioridade.h"
-#include "leitura_txt.h"
+#include "leitura_txt.h" 
 
 int main(int argc, char *argv[]){
 
 	int N = QuantProc(argv[1]) ;
-        printf("\nNumero de processos recebidos = %d\n",N);//DEBUG
+	printf("\nNumero de processos = %d\n",N);//DEBUG
 	processo *p;
 	p = malloc(N*sizeof(processo));
 	TxtRead(N,p,argv[1]);
 
 	prioridadeNP(N,p);
 
-    /*
-    for (int i=0;i<N;i++){      //DEBUG -  printando struct BCP
-        printf("o nome do processo %i eh: %s\n",i,p[i].nome);
-        printf("o burst do processo %i eh: %i\n",i,p[i].burst);
-        printf("a prioridade do processo %i eh: %i\n\n",i,p[i].prioridade);
-    }
-    */
+	/*for (int i=0;i<N;i++){ //DEBUG -  printando struct BCP
+		printf("o nome do processo %i eh: %s\n",i,p[i].nome);
+		printf("o burst do processo %i eh: %i\n",i,p[i].burst);
+		printf("a prioridade do processo %i eh: %i\n\n",i,p[i].prioridade);
+	}*/
 
-    return 0;
+	return 0;
 }
--- Trabalho 1/recurso.c
+++ Trabalho 1/recurso.c
@@ -1,9 +1,9 @@
 /*
 INTEGRANTES:
-Nome: Eduardo Marques da Silva          - Número: 11721EMT018
-Nome: Enrico Sampaio Bonela             - Número: 11721EMT007
-Nome: Guilherme Salomao Agostini        - Número: 11721EMT003
-Nome: Luiz Renato Rodrigues Carneiro    - Número: 11721EMT004
+Nome: Eduardo Marques da Silva - Número: 11721EMT018
+Nome: Enrico Sampaio Bonela - Número: 11721EMT007
+Nome: Guilherme Salomao Agostini - Número: 11721EMT003
+Nome: Luiz Renato Rodrigues Carneiro - Número: 11721EMT004
 */
 
 #include <stdio.h>
@@ -18,50 +18,51 @@ Nome: Luiz Renato Rodrigues Carneiro    - Número: 11721EMT004
 
 
 
-void escalonador(int N, processo *pointer, int escolha){
-    /*
-    for (int i=0;i<N;i++){ //DEBUG -  printando struct BCP
-        printf("o nome do processo %i eh: %s\n",i,pointer[i].nome);
-        printf("o burst do processo %i eh: %i\n",i,pointer[i].burst);
-        printf("a prioridade do processo %i eh: %i\n\n",i,pointer[i].prioridade);
-    }
-    */
+void escalonador(int N, processo *pointer, int escolha){ 
+	/*
+	for (int i=0;i<N;i++){ //DEBUG -  printando struct BCP
+		printf("o nome do processo %i eh: %s\n",i,pointer[i].nome);
+		printf("o burst do processo %i eh: %i\n",i,pointer[i].burst);
+		printf("a prioridade do processo %i eh: %i\n\n",i,pointer[i].prioridade);
+	}*/
 
-    if (escolha==1){
-        FCFS(N,pointer);
-    }
-    else if (escolha==2){
-        prioridadeNP(N,pointer);
-    }
-    else{
-        printf("\n## ERRO ##\nNão existe essa opção!\n");
-    }
+	if (escolha==1){       
+		FCFS(N,pointer);
+	}
+	else if (escolha==2){       
+		prioridadeNP(N,pointer);
+	}
+	else{
+		printf("\nERRO: nao existe essa opcao.\n");
+	}
 
 }
+
 
 int main (int argc, char *argv[]){
 
-    int N = QuantProc("tarefas.txt") ;
-    printf("\nN = %d\n",N);//DEBUG
-    processo *p;
-    p = malloc(N*sizeof(processo));
-    TxtRead(N, p, "tarefas.txt");
 
-    int escolha;
-    printf("Escolha o escalonador desejado [1] ou [2]:\n 1 - First Come First Served (FCFS)\n 2 - Prioridade Não-Preemptivo\n");
-    scanf("%d", &escolha);
-
-    escalonador(N,p,escolha);
-
-    /*
-    for (int i=0;i<N;i++){ //DEBUG -  printando struct BCP
-        printf("o nome do processo %i eh: %s\n",i,p[i].nome);
-        printf("o burst do processo %i eh: %i\n",i,p[i].burst);
-        printf("a prioridade do processo %i eh: %i\n\n",i,p[i].prioridade);
-    }
-    */
-
-    return 0;
-}
+	int N = QuantProc("tarefas.txt") ;
+	printf("\nN = %d\n",N);//DEBUG
+	processo *p;
+	p = malloc(N*sizeof(processo));
+	TxtRead(N, p, "tarefas.txt");
 
 
+
+	int escolha;
+	printf("Escolha o escalonador desejado [1] ou [2]:\n 1 - First Come First Served (FCFS)\n 2 - Prioridade Não-Preemptivo\n");
+	scanf("%d", &escolha);
+
+	escalonador(N,p,escolha);
+
+	/*   for (int i=0;i<N;i++){ //DEBUG -  printando struct BCP
+	printf("o nome do processo %i eh: %s\n",i,p[i].nome);
+	printf("o burst do processo %i eh: %i\n",i,p[i].burst);
+	printf("a prioridade do processo %i eh: %i\n\n",i,p[i].prioridade);
+	}*/
+
+	return 0;
+}  
+
+    
